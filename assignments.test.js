d2d09318c985a53a7881168eba3dc239ff3af7d5
Moh Oktavi Aziz Nugraha <moktavizzen@gmail.com>
2026-04-03 12:58:06 +0700

    Use `describe()` for clear hierarchy

--- assignments.test.js
+++ assignments.test.js
@@ -1,70 +1,84 @@
-import { expect, test } from "vitest";
+import { describe, expect, test } from "vitest";
 import { calculator, capitalize, reverseString } from "./assignments.js";
 
-test("capitalize(): Only accept string", () => {
-  expect(() => capitalize(67)).toThrow("only accept string");
-  expect(() => capitalize(true)).toThrow("only accept string");
+describe("capitalize()", () => {
+  test("Only accept string", () => {
+    expect(() => capitalize(67)).toThrow("only accept string");
+    expect(() => capitalize(true)).toThrow("only accept string");
+  });
+
+  test("Capitalize the first character", () => {
+    expect(capitalize("foo")).toBe("Foo");
+  });
+
+  test("Works with different inputs", () => {
+    expect(capitalize("bar")).toBe("Bar");
+    expect(capitalize("0bar")).toBe("0bar");
+  });
 });
 
-test("capitalize(): Capitalize the first character", () => {
-  expect(capitalize("foo")).toBe("Foo");
+describe("reverseString()", () => {
+  test("Only accept string", () => {
+    expect(() => reverseString(67)).toThrow("only accept string");
+    expect(() => reverseString(true)).toThrow("only accept string");
+  });
+
+  test("Reverse the string", () => {
+    expect(reverseString("Foo")).toBe("ooF");
+  });
+
+  test("Works with different inputs", () => {
+    expect(reverseString("Bar")).toBe("raB");
+    expect(reverseString("0Bar")).toBe("raB0");
+  });
 });
 
-test("capitalize(): Works with different inputs", () => {
-  expect(capitalize("bar")).toBe("Bar");
-  expect(capitalize("0bar")).toBe("0bar");
-});
+describe("calculator", () => {
+  describe("add()", () => {
+    test("Only accept Numbers", () => {
+      expect(() => calculator.add("one", "two")).toThrow("only accept numbers");
+      expect(() => calculator.add("8", 8)).toThrow("only accept numbers");
+    });
 
-test("reverseString(): Only accept string", () => {
-  expect(() => reverseString(67)).toThrow("only accept string");
-  expect(() => reverseString(true)).toThrow("only accept string");
-});
+    test("Calculate x + y", () => {
+      expect(calculator.add(2, 2)).toBe(4);
+      expect(calculator.add(4, -1)).toBe(3);
+    });
+  });
 
-test("reverseString(): Reverse the string", () => {
-  expect(reverseString("Foo")).toBe("ooF");
-});
+  describe("subtract()", () => {
+    test("Only accept Numbers", () => {
+      expect(() => calculator.subtract("one", "two")).toThrow("only accept numbers");
+      expect(() => calculator.subtract("8", 8)).toThrow("only accept numbers");
+    });
 
-test("reverseString(): Works with different inputs", () => {
-  expect(reverseString("Bar")).toBe("raB");
-  expect(reverseString("0Bar")).toBe("raB0");
-});
+    test("Calculate x - y", () => {
+      expect(calculator.subtract(2, 2)).toBe(0);
+      expect(calculator.subtract(4, -1)).toBe(5);
+    });
+  });
 
-test("calculator.add(): Only accept Numbers", () => {
-  expect(() => calculator.add("one", "two")).toThrow("only accept numbers");
-  expect(() => calculator.add("8", 8)).toThrow("only accept numbers");
-});
+  describe("divide()", () => {
+    test("Only accept Numbers", () => {
+      expect(() => calculator.divide("one", "two")).toThrow("only accept numbers");
+      expect(() => calculator.divide("8", 8)).toThrow("only accept numbers");
+    });
 
-test("calculator.add(): Calculate x + y", () => {
-  expect(calculator.add(2, 2)).toBe(4);
-  expect(calculator.add(4, -1)).toBe(3);
-});
+    test("Calculate x / y", () => {
+      expect(calculator.divide(2, 2)).toBe(1);
+      expect(calculator.divide(4, -1)).toBe(-4);
+    });
+  });
 
-test("calculator.subtract(): Only accept Numbers", () => {
-  expect(() => calculator.subtract("one", "two")).toThrow("only accept numbers");
-  expect(() => calculator.subtract("8", 8)).toThrow("only accept numbers");
-});
+  describe("multiply()", () => {
+    test("Only accept Numbers", () => {
+      expect(() => calculator.multiply("one", "two")).toThrow("only accept numbers");
+      expect(() => calculator.multiply("8", 8)).toThrow("only accept numbers");
+    });
 
-test("calculator.subtract(): Calculate x - y", () => {
-  expect(calculator.subtract(2, 2)).toBe(0);
-  expect(calculator.subtract(4, -1)).toBe(5);
-});
-
-test("calculator.divide(): Only accept Numbers", () => {
-  expect(() => calculator.divide("one", "two")).toThrow("only accept numbers");
-  expect(() => calculator.divide("8", 8)).toThrow("only accept numbers");
-});
-
-test("calculator.divide(): Calculate x / y", () => {
-  expect(calculator.divide(2, 2)).toBe(1);
-  expect(calculator.divide(4, -1)).toBe(-4);
-});
-
-test("calculator.multiply(): Only accept Numbers", () => {
-  expect(() => calculator.multiply("one", "two")).toThrow("only accept numbers");
-  expect(() => calculator.multiply("8", 8)).toThrow("only accept numbers");
-});
-
-test("calculator.multiply(): Calculate x * y", () => {
-  expect(calculator.multiply(2, 2)).toBe(4);
-  expect(calculator.multiply(4, -1)).toBe(-4);
+    test("Calculate x * y", () => {
+      expect(calculator.multiply(2, 2)).toBe(4);
+      expect(calculator.multiply(4, -1)).toBe(-4);
+    });
+  });
 });
